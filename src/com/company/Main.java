da33a68731ff519208261646527eb253c4212065
Yoga Pratama Firmansyah <37876340+yogasyah@users.noreply.github.com>
2018-03-29 08:58:59 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,23 @@
-package com.company;
-
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-            Scanner show = new Scanner(System.in);
-            System.out.print("Masukan Jumlah Deret Fibonacci : ");
-            int n = show.nextInt();
-            long fib[] = new long[n];
+        Scanner show = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Deret Fibonacci : ");
+        int n = show.nextInt();
+        long fib[] = new long[n];
 
-            fib[0] = 1;
-            fib[1] = 1;
+        fib[0] = 1;
+        fib[1] = 1;
 
-            for(int i = 2; i < n; i++) {
-                fib[i] = fib[i-1] + fib[i-2];
-            }
-
-            for (int i = 0; i < n; i++) {
-                System.out.print(fib[i] +  " ");
-            }
+        for(int i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
         }
-	// write your code here
+        System.out.println("Jumlah : ");
+        long sum = 0;
+        for (int i = 0; i < n; i++){
+            sum = fib[i] + sum;
+        }
+        System.out.println(sum);
     }
+}
